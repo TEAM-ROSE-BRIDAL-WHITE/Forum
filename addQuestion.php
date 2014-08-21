276ea3a50408534eae3cc5caa5c9798dd7c7f4c6
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-08-21 23:06:22 +0300

    Create addQuestion.php

---
+++ addQuestion.php
@@ -0,0 +1,11 @@
+<?php
+include_once("db.php");
+class AddQuestions extends dBconn{
+    private function add($name, $content, $id){
+        parent::addSomething(
+            'posts',
+            array('name', 'content', 'user_id', 'timeadded', 'cat_id'),
+            array($name, $content, $_SESSION['admin'], time(), $id));
+    }
+}
+?>
