f5696ffa5f53d5f6c4153efae745a9419a0504b6
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-08-21 22:45:12 +0300

    Create db.sql
    Our database. It contains "users", "categories", "postings" and "tags".
    Where posts has a foreign key "user_id" which says which user is the author and "cat_id" which says from which category is current post.
    Tags has a foreign key "post_id" which says from which post is the tag.

---
+++ db.sql
@@ -0,0 +1,38 @@
+create database forum;
+
+use forum;
+
+create table if not exists users(
+	user_id int not null auto_increment primary key,
+	permission int not null, /*prava za dostap (user, admin, moderator, ...)*/
+	username varchar(255) not null,
+	password varchar(255) not null,
+	mail varchar(255) not null
+);
+
+create table if not exists categories(
+	cat_id int not null auto_increment primary key,
+	name varchar(255) not null
+);
+
+create table if not exists posts(
+	post_id int not null auto_increment primary key,
+	name varchar(255) not null COLLATE utf8_unicode_ci,
+	content text not null COLLATE utf8_unicode_ci,
+	user_id int not null, /*ot koi user e dobavena*/
+	cat_id int not null, /*kam koq kategoriq e saotvetniqt podting (vapros)*/
+	timeadded int not null,
+	fromcat varchar(255) not null,
+	timeedited int not null,
+	editfromwho int not null,
+	visits int not null,
+	foreign key (user_id) references users(user_id),
+	foreign key (cat_id) references categories(cat_id)
+);
+
+create table if not exists tags(
+	tag_id int not null auto_increment primary key,
+	post_id int not null,
+	tagname varchar (255) not null,
+	foreign key (post_id) references posts(post_id)
+);
